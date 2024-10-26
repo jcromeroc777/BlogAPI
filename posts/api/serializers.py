cf24b8a3d70c4d824e46224d3a2a21aa5665d1f0
Julio Romero <jcromeroc.19@gmail.com>
2024-10-26 18:32:56 -0400

    feat: filtros

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -4,4 +4,4 @@ from posts.models import Post
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'order', 'created_at']
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,10 +1,28 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 from posts.api.permissions import IsAdminOrReadOnly
 
 class PostModelViewSet(ModelViewSet):
+    ordering = ['-title']
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    # http_method_names = ['get'] sirve para restringir los métodos que se pueden usar en la vista
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['title']
+    ordering_fields = ['order']
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='order-one')
+    def order1(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().filter(order=1))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
